fccbd3b718359b994b0a104d9a89f69f88f4156a
Yash <yashsolanki0728@gmail.com>
2026-01-29 17:11:48 +0530

    bug fixes

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
-import express from "express";
-import cors from "cors";
-import cookieParser from "cookie-parser";
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
 
-const app = express();
+const app = express()
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
 }))
 
 app.use(express.json({limit: "16kb"}))
@@ -14,10 +15,10 @@ app.use(express.static("public"))
 app.use(cookieParser())
 
 
-// routes import
-import userRouter from "./routes/user.routes.js"
+//routes import
+import userRouter from './routes/user.routes.js'
 
-// routes declaration
-app.use("/api/v1/users", userRouter);
+//routes declaration
+app.use("/api/v1/users", userRouter)
 
-export default app;
+export { app }
