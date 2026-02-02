00627b2e61045d9043335ac35c8282b4fca3f0ac
Yash <yashsolanki0728@gmail.com>
2026-02-02 22:05:47 +0530

    refactor: comment out unused tweet and likedBy fields in Like schema

--- src/models/like.model.js
+++ src/models/like.model.js
@@ -10,14 +10,14 @@ const likeSchema = new Schema({
         type: Schema.Types.ObjectId,
         ref: "Comment"
     },
-    tweet: {
-        type: Schema.Types.ObjectId,
-        ref: "Tweet"
-    },
-    likedBy: {
-        type: Schema.Types.ObjectId,
-        ref: "User"
-    },
+    // tweet: {
+    //     type: Schema.Types.ObjectId,
+    //     ref: "Tweet"
+    // },
+    // likedBy: {
+    //     type: Schema.Types.ObjectId,
+    //     ref: "User"
+    // },
     
 }, {timestamps: true})
 
